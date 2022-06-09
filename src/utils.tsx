a1872f1b3a9f998918e42227f02e530aeec69624
Fisch03 <derfisch03@gmail.com>
2022-06-10 01:49:27 +0200

    Clean up old executeInTab code

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,27 +1,16 @@
 import { ServerAPI } from "decky-frontend-lib";
 
-export const sc   = async (sAPI: ServerAPI, action: string) => sAPI.executeInTab("SP", true, `SteamClient.${action}`);
-export const apps = async (sAPI: ServerAPI, action: string) => sAPI.executeInTab("SP", true,        `Apps.${action}`);
-
-export const createShortcut = async (sAPI: ServerAPI, name: string) => {
-    let result:any = await sc(sAPI, `Apps.AddShortcut("${name}","/usr/bin/flatpak")`)
-    let id:number = result.result.result;
+export const createShortcut = (name: string) => {
+    //@ts-ignore
+    let id:number = SteamClient.Apps.AddShortcut(name,"/usr/bin/flatpak")
     return id
 }
 
-export const gameIDFromAppID = async (sAPI:ServerAPI, appid: number) => {
-    //executing most of js in the tab avoids some errors i cannot explain myself
-    let result:any = await sAPI.executeInTab("SP", true, `
-        (async () => {
-            let res = await appStore.GetAppOverviewByAppID(${appid})
-            return JSON.stringify(res)
-        })();
-    `)
-    
-    let game = result.result.result; //yes
+export const gameIDFromAppID = (appid: number) => {
+    //@ts-ignore
+    let game = appStore.GetAppOverviewByAppID(appid);
 
-    if(game != "null") {
-        game = JSON.parse(game);
+    if(game !== null) {
         return game.m_gameid;
     } else {
         return -1
@@ -38,11 +27,13 @@ export const fetchApps = async (sAPI: ServerAPI) => {
 export const launchApp = async (sAPI: ServerAPI, packageName: string) => {
     let id: number = await getShortcutID(sAPI);             
   
-    sc(sAPI, `Apps.SetShortcutLaunchOptions(${id}, "run ${packageName}")`); //This does not apply immediately and also cannot be awaited!
+    //@ts-ignore
+    SteamClient.Apps.SetShortcutLaunchOptions(id, `run ${packageName}`); //This does not apply immediately and also cannot be awaited!
     setTimeout(async () => {
-        let gid = await gameIDFromAppID(sAPI, id);
-        sc(sAPI, `Apps.RunGame("${gid}","",-1,100)`);
-    }, 100)
+        let gid = await gameIDFromAppID(id);
+        //@ts-ignore
+        SteamClient.Apps.RunGame(gid,"",-1,100);
+    }, 500)
   }
 
 export const getShortcutID = async (sAPI: ServerAPI) => {
@@ -52,10 +43,10 @@ export const getShortcutID = async (sAPI: ServerAPI) => {
         let id: number = result.result;
 
         if(id == -1) {
-            id = await createShortcut(sAPI, "QuickLaunch");
+            id = await createShortcut("QuickLaunch");
             sAPI.callPluginMethod("set_id", {id: id});
-        } else if(await gameIDFromAppID(sAPI, id) == -1) {
-            id = await createShortcut(sAPI, "QuickLaunch");
+        } else if(await gameIDFromAppID(id) == -1) {
+            id = await createShortcut("QuickLaunch");
             sAPI.callPluginMethod("set_id", {id: id});
         }
 
